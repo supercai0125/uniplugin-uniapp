8dae4021972dc11a1a301629fe9e520de248b028
caishiqi <1403317572@qq.com>
2021-05-21 17:13:09 +0800

    uniplugin-uniapp demo

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="container">
+		<button type="default" @click="watchUpdate">手表更新</button>
+	</view>
+</template>
+
+<script>
+	var PhoneBle = uni.requireNativePlugin("Phone-Ble");
+	export default {
+		methods: {
+			watchUpdate() {
+				PhoneBle.gotoNativePage()
+			},
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
